c085aeb6d04ba97c7ccb4e904583ac59bf82a192
Jonathan Ankiewicz <jankiewicz@ten-x.com>
2017-11-12 20:17:07 -0800

    saving place taking over baby duty

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # -fauxMotivation
+
+
+
+Just a few notes while I develop
+
+      docs https://dev.twitter.com/twitterkit/android/log-in-with-twitter#android-guest-login
+ 
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+const T = require('./app/tweet.js')
+
+app.get('/', (req, res) => {
+  T.get('statuses/user_timeline', { screen_name: 'FauxMotivation', count: 2 },  function (err, data, response) {
+    res.send(data)
+  })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
